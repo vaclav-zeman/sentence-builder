738594e66e9d91ffad6fd737a28c80239e0b2a5a
Vaclav Zeman <vaclav.zeman@avast.com>
2019-06-14 22:22:08 +0200

    Update Sentence

--- src/features/Sentence.tsx
+++ src/features/Sentence.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { IStore } from '../redux/rootReducer'
+import { FormValues } from '../ui/Form'
 import Title from '../ui/Title'
 
-const Sentence = ({ values }: any) => {
-  const sentence = Object.values(values)
-    .map(value => (value === '' ? '-----' : value))
-    .join(' ')
-
-  return <Title>{sentence}</Title>
+const Sentence = ({ values }: FormValues) => {
+  return (
+    <Title>
+      {Object.entries(values).map(([key, value]) => (
+        <span key={key}>{value || '----'} </span>
+      ))}
+    </Title>
+  )
 }
 
 export default connect((state: IStore) => ({ values: state.Form.values }))(Sentence)
