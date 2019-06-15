841e17a94888285df0c3f13254b19d3375f1565f
Vaclav Zeman <vaclav.zeman@avast.com>
2019-06-15 11:20:10 +0200

    Move App

--- src/features/App.tsx
+++ src/features/App.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
 
-import GlobalStyles from './ui/GlobalStyles'
-import Layout from './ui/Layout'
-import store from './redux/store'
-import StepForm from './features/StepForm'
-import theme from './theme'
-import Sentence from './features/Sentence'
+import GlobalStyles from '../ui/GlobalStyles'
+import Layout from '../ui/Layout'
+import Sentence from './Sentence'
+import StepForm from './StepForm'
+import store from '../redux/store'
+import theme from '../theme'
 
 const App: React.FC = () => {
   return (
