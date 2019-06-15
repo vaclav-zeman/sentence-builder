463fe56d527d7ba43fbdec92e3ae4a6ea7984831
Vaclav Zeman <vaclav.zeman@avast.com>
2019-06-15 11:15:34 +0200

    Updates

---
+++ precache-manifest.a5444bda85665550495d47ae2195b447.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07d09fa6ae769a0f80852cf85cc1f23",
+    "url": "/sentence-builder/index.html"
+  },
+  {
+    "revision": "0a2e147b06fc4537df6b",
+    "url": "/sentence-builder/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "556440c68d7a85e618de",
+    "url": "/sentence-builder/static/js/2.d6d53bd1.chunk.js"
+  },
+  {
+    "revision": "0a2e147b06fc4537df6b",
+    "url": "/sentence-builder/static/js/main.a9e2647f.chunk.js"
+  },
+  {
+    "revision": "f02ea5cea5e59a54c23d",
+    "url": "/sentence-builder/static/js/runtime~main.35ccc586.js"
+  }
+]);
